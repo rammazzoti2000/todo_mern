0cbb4bc54242f28e0a0dfa331fdad77e6a6da41b
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-10-30 15:17:43 +0100

    Implement Delete API Controller

--- src/controllers/todos/index.ts
+++ src/controllers/todos/index.ts
@@ -52,3 +52,21 @@ const updateTodo = async (req: Request, res: Response): Promise<void> => {
     throw error
   }
 }
+
+const deleteTodo = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const deletedTodo: ITodo | null = await Todo.findByIdAndRemove(
+      req.params.id
+    )
+    const allTodos: ITodo[] = await Todo.find()
+    res.status(200).json({
+      message: "Todo deleted",
+      todo: deletedTodo,
+      todos: allTodos,
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+export { getTodos, addTodo, updateTodo, deleteTodo }
