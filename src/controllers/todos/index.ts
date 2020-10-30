84f9b107cffdb0172bbc57383ecaa66efdebda69
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-10-30 14:57:53 +0100

    Implement Get API controller

---
+++ src/controllers/todos/index.ts
@@ -0,0 +1,14 @@
+import { Response, Request } from 'express';
+import { ITodo} from './../../types/todo';
+import Todo from '../../models/todo';
+
+const getTodos = async (req: Request, res:Response): Promise<void> => {
+  try {
+    const todos: ITodo[] = await Todo.find()
+    res.status(200).json({ todos })
+  } catch (error) {
+    throw error
+  }
+}
+
+
