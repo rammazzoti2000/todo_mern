bdf3c87affb71cf4a7f035ec4c585663f8788716
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-11-02 12:19:34 +0100

    client: Implement addTodo() method to API

--- front-client/src/API.ts
+++ front-client/src/API.ts
@@ -13,3 +13,22 @@ export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
     throw new Error(error)
   }
 }
+
+export const addTodo = async (
+  formData: ITodo
+): Promise<AxiosResponse<ApiDataType>> => {
+  try {
+    const todo: Omit<ITodo, "_id"> = {
+      name: formData.name,
+      description: formData.description,
+      status: false,
+    }
+    const saveTodo: AxiosResponse<ApiDataType> = await axios.post(
+      baseUrl + "/addTodo",
+      todo
+    )
+    return saveTodo
+  } catch (error) {
+    throw new Error(error)
+  }
+}
